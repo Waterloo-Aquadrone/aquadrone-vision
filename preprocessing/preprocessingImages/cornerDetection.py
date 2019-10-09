b9912e8a7ea413b8dc07db2c8a3cf18cfb34074c
Maxine Zhang <meixinzhang@outlook.com>
2019-10-09 11:12:54 +0200

    wrap the script in a function

--- preprocessing/preprocessingImages/cornerDetection.py
+++ preprocessing/preprocessingImages/cornerDetection.py
@@ -2,47 +2,55 @@ import math
 import numpy as np
 import cv2 as cv
 
-filename = 'test.jpg'
-img = cv.imread(filename)
-
-# convert the input image into grayscale color space 
-grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
-
-# modify the data type setting to 32-bit floating point 
-operatedImage = np.float32(grayImage) 
-cv.imshow('operatedImage',operatedImage)
-
-dst = cv.cornerHarris(operatedImage, 2, 5, 0.07)
-
-
-# finding the cordinates of corners
-bwCorners = np.zeros_like(operatedImage)        
-bwCorners[dst > 0.01*dst.max()] = 255
-
-coor_array = np.argwhere(bwCorners)
-coor_list = [l.tolist() for l in list(coor_array)]
-coor_tuples = [tuple(l) for l in coor_list]
-
-
-# filter out the coordinates that are close (needs some modification)
+# helper for filtering points
 def distance(pt1, pt2):
-    (x1, y1), (x2, y2) = pt1, pt2
-    dist = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
-    return dist
-
-i = 1
-thresh = 10
-for pt1 in coor_tuples:
-    for pt2 in coor_tuples[i::1]:
-        if (distance(pt1, pt2) < thresh):
-            coor_tuples.remove(pt2)      
-    i += 1
+        (x1, y1), (x2, y2) = pt1, pt2
+        dist = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
+        return dist
 
 
-# show the final points on a copy of the image
-img2 = img.copy()
-for pt in coor_tuples:
-    print(pt)
-    cv.circle(img2, tuple(reversed(pt)), 3, (0, 0, 255), -1)
-cv.imshow('Image with main corners', img2) 
-# cv.waitKey(0)
+def get_contours(filename):
+    img = cv.imread(filename)
+
+    # convert the input image into grayscale color space 
+    grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+
+    # modify the data type setting to 32-bit floating point and get corners 
+    operatedImage = np.float32(grayImage) 
+    dst = cv.cornerHarris(operatedImage, 2, 5, 0.07)
+
+
+    # finding the cordinates of corners
+    bwCorners = np.zeros_like(operatedImage)        
+    bwCorners[dst > 0.01*dst.max()] = 255
+
+    coor_array = np.argwhere(bwCorners)
+    coor_list = [l.tolist() for l in list(coor_array)]
+    coor_tuples = [tuple(l) for l in coor_list]
+
+
+    # filter out the coordinates that are close 
+    # (here I just took the first point, for improvement we can take center point)
+    i = 1
+    thresh = 10
+    for pt1 in coor_tuples:
+        for pt2 in coor_tuples[i::1]:
+            if (distance(pt1, pt2) < thresh):
+                coor_tuples.remove(pt2)      
+        i += 1
+
+
+    # show the final points on a copy of the image
+    '''
+    img2 = img.copy()
+    for pt in coor_tuples:
+        cv.circle(img2, tuple(reversed(pt)), 3, (0, 0, 255), -1)
+    cv.imshow('Image with main corners', img2)
+    cv.waitKey(0)
+    '''
+
+    return coor_tuples
+
+
+# corners = get_contours("test.jpg")
+# print(corners)
