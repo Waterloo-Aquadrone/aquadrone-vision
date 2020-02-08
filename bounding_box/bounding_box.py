68f76564b388f79ddc3dd53b6bafcc7e97a98c9a
Yash Arora <YashArora0606@users.noreply.github.com>
2020-02-08 16:56:51 -0500

    Create bounding_box.py

---
+++ bounding_box/bounding_box.py
@@ -0,0 +1,10 @@
+# Class to create, train, and predict anything related to boundingboxes
+class BoundingBox():
+    def train_model():
+        return 0
+    def predict(img):
+        return 0
+    def load_model():
+        return 0
+    def save_model():
+        return 0
