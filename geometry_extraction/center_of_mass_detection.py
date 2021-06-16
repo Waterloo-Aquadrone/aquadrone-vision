ee55a53ab6d6e6d1d3cddf995f7d0bf86e39a1e8
msmel01 <ms.elly17.01@gmail.com>
2021-06-16 04:11:34 -0400

    debug + tweak image processing techniques

--- geometry_extraction/center_of_mass_detection.py
+++ geometry_extraction/center_of_mass_detection.py
@@ -1,20 +1,28 @@
 import cv2
 import numpy as np
 import sys, argparse
-import random as rnd
-from functools import partial
+
+# needs work to find optimal algorithms and parameters
+# find ways to improve image quality before contouring and finding the center
 
 def find_center_of_gate(img):
     pass
 
 
-def find_center_of_blob(thresh):
+def find_center_of_blob():
 
-    print("new function call with threshhold set to {}".format(thresh))
+    # print("new function call with threshhold set to {}".format(thresh))
     img_copy = src_img.copy()
 
-    canny_output = cv2.Canny(src_img, thresh, thresh * 2) # canny edge detection
-    contours,_ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # best algorithm?
+    img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.blur(img_gray, (3,3))
+
+    img_thresh = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
+    # canny_output = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5)
+
+
+    # canny_output = cv2.Canny(img_gray, thresh, thresh * 2) # canny edge detection
+    contours,hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     num_contours = len(contours)
 
     if (num_contours > 0):
@@ -27,20 +35,14 @@ def find_center_of_blob(thresh):
             # add 1e-5 to avoid division by zero -> opencv tutorial method
             mass_centers[i] = (mu[i]['m10'] / (mu[i]['m00'] + 1e-5), mu[i]['m01'] / (mu[i]['m00'] + 1e-5))
 
-        # drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-        #
-
-        #cv2.drawContours(src_img, contours, -1, (0,0,255), 3)
+        # print(hierarchy)
 
         for i in range(num_contours):
-            # color = (rnd.randint(0,256), rnd.randint(0,256), rnd.randint(0,256))
-            color = (0, 0, 255)
-            color_center = (0, 255, 0)
-            cv2.drawContours(img_copy, contours, i, color, 2)
-            print(int(mass_centers[i][0]))
-            print(int(mass_centers[i][1]))
-            print("object {}".format(i))
-            cv2.circle(img_copy, (int(mass_centers[i][0]), int(mass_centers[i][1])), 10, color_center, -1)
+            if (hierarchy[0][i][3] == -1):
+                color_contour = (0, 0, 255)
+                color_center = (0, 255, 0)
+                cv2.drawContours(img_copy, contours, i, color_contour, 2)
+                cv2.circle(img_copy, (int(mass_centers[i][0]), int(mass_centers[i][1])), 2, color_center, -1)
 
         cv2.imshow("Image", img_copy)
 
@@ -64,18 +66,16 @@ def main(argv):
     new_size = (width, height)
     src_img = cv2.resize(src_img, new_size)
 
-    img_gray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
-    img_gray = cv2.blur(img_gray, (3,3))
-    # figure out what image processing technique is optimal
-
     src_window = 'Image'
     cv2.namedWindow(src_window)
     cv2.imshow(src_window, src_img)
 
-    max_thresh = 255
-    thresh = 100 # initial threshold
-    cv2.createTrackbar('Canny Thresh:', src_window, thresh, max_thresh, find_center_of_blob)
-    find_center_of_blob(thresh)
+    # max_thresh = 255
+    # thresh = 100 # initial threshold
+    # cv2.createTrackbar('Canny Thresh:', src_window, thresh, max_thresh, find_center_of_blob)
+    # find_center_of_blob(thresh)
+
+    find_center_of_blob()
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
